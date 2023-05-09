74b49d4965465e4f5685120b7d7918f231b3451d
Brian Whisler <brian.whisler@gmail.com>
2023-05-08 21:12:42 -0600

    more logging

--- devices/switch.js
+++ devices/switch.js
@@ -40,12 +40,13 @@ basicSwitch.prototype={
 		this.wallboxapi.getChargerData(this.platform.token,device.id).then(response=>{
 			try{
 				statusCode=response.data.data.chargerData.status
-				this.log.warn(statusCode)
 				currentMode=this.enumeration.items.filter(result=>result.status == statusCode)[0].mode
-				this.log.warn(currentMode)
 				this.log.debug('checking current mode = %s',currentMode)
 			}catch(error){
 				currentMode='unknown'
+				statusCode=response.data.data.chargerData.status
+				this.log.warn(statusCode)
+				this.log.warn(error)
 				this.log.error('failed current mode check')
 			}
 			switch(currentMode){
